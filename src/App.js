03060842c89c22c1bf14ce3ee80b71b05be54e88
BurakPC <esadburak@yahoo.com.tr>
2019-08-29 11:26:26 +0300

    Aligned Sidebyside

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import StoreViewer from './StoreViewer.js';
-import AddButton from './AddButton.js';
-import InputPlace from './InputPlace.js'
-
 export default class App extends React.Component{
   render() {
     return (
